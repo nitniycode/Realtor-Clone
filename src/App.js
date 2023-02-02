fd21bdbd9d0824528cdc513cad502844fd51241a
nitin yadav <ny941078@gmail.com>
2023-02-02 14:29:57 +0530

    Created Pages and Installed React-Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ForgotPassword from "./Pages/ForgotPassword";
+import Home from "./Pages/Home";
+import Offers from "./Pages/Offers";
+import Signin from "./Pages/Signin";
+import Signup from "./Pages/Signup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </>
+    
   );
 }
 
